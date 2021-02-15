6a3664fc13a891ac51c0c0101bc09a1c6540f44f
Ubuntu <ubuntu@ip-172-31-46-227.us-west-2.compute.internal>
2021-02-15 01:36:58 +0000

    Initial commit

---
+++ reltag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+touch test.txt
+git init
+git add .
+git commit -m "Initial commit"
+git flow init
+[defaults accepted]
+git flow release start 1.0
+echo "Line 1" >> test.txt
+git add .
+git commit -am "Line added"
+git flow release finish '1.0'
